acd84c2907e408fed91cbf9f5096752d91bfbbc5
MarkusEfr <you@example.com>
2018-11-01 09:30:21 +0200

    Dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "https://github.com/MarkusEfr/Office.git"
 # set :pty, true
 
 # Default value for :linked_files is []
- append :linked_files, "config/database.yml"
+ append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
  append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
